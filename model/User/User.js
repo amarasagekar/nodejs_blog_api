160cd9df7e00c16039184b30bc62ac853f80fdf5
Amar Asagekar <amarasagekar@gmail.com>
2024-02-08 16:21:27 +0530

    added virtual properties to get count

--- model/User/User.js
+++ model/User/User.js
@@ -85,7 +85,37 @@ const userSchema = new mongoose.Schema(
 
 //Get Fullname
 userSchema.virtual("fullname").get(function () {
-  return `${this.firstname} ${this.lastname}`
+  return `${this.firstname} ${this.lastname}`;
+});
+
+//Get user initials
+userSchema.virtual("initials").get(function () {
+  return `${this.firstname[0]}${this.lastname[0]}`;
+});
+
+// Get postCount
+userSchema.virtual("postCounts").get(function () {
+  return this.posts.length;
+});
+
+//Get follower count
+userSchema.virtual("followersCount").get(function () {
+  return this.followers.length;
+});
+
+//get following count
+userSchema.virtual("followingCount").get(function () {
+  return this.following.length;
+});
+
+//get viewers count
+userSchema.virtual("viewersCount").get(function () {
+  return this.viewers.length;
+});
+
+//get blocked count
+userSchema.virtual("blockedCount").get(function () {
+  return this.blocked.length;
 });
 //Compile the user model
 const User = mongoose.model("User", userSchema);
